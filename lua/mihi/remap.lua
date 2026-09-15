e6e2d1cc2a70866e424f968f330f603efb3a2776
Mihir Naik <naikmihi@msu.edu>
2026-09-15 18:20:38 -0400

    changes

--- lua/mihi/remap.lua
+++ lua/mihi/remap.lua
@@ -1,5 +1,9 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>e", function() require("oil").open() end, { desc = "Oil (files)" })
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Oil parent directory" })
-vim.keymap.set("i", "jk", "<Esc>")
+vim.keymap.set("i", "jk", "<Esc>l")
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear search highlight" })
 
+vim.keymap.set("n", "<C-s>", "<cmd>update<CR>", {silent = true})
+vim.keymap.set("v", "<C-s>", "<cmd>update<CR>", {silent = true})
+vim.keymap.set("i", "<C-s>", "<cmd>update<CR>", {silent = true})
--- lua/plugins/latex.lua
+++ lua/plugins/latex.lua
@@ -6,5 +6,13 @@ return {
 			vim.g.vimtex_view_automatic = 0
 			vim.g.vimtex_compiler_method = 'latexmk'
 		end,
+		build = function()
+			local patch = vim.fn.stdpath('config') .. '/patches/vimtex-compiler-clash-guard.patch'
+			local dir = vim.fn.stdpath('data') .. '/lazy/vimtex'
+			local check = vim.fn.system('git -C ' .. vim.fn.shellescape(dir) .. ' apply --check --whitespace=nowarn ' .. vim.fn.shellescape(patch))
+			if check == '' then
+				vim.fn.system('git -C ' .. vim.fn.shellescape(dir) .. ' apply --whitespace=nowarn ' .. vim.fn.shellescape(patch))
+			end
+		end,
 	},
 }
